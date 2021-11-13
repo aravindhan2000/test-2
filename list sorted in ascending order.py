78be7bc993e1df0b826a5c0b05df5ddd0c86c7ff
aravindhan2000 <93928239+aravindhan2000@users.noreply.github.com>
2021-11-13 15:48:45 +0530

    assignment 2

---
+++ list sorted in ascending order.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[115]:
+
+
+def last(n):
+    return n[m]
+
+def sort(tuples):
+    return sorted(tuples, key = last)
+
+a = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+m = 1
+print("Sorted:"),
+print(sort(a))
+
+
+# In[ ]:
+
+
+
+
